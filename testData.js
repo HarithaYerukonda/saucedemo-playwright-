83f8e58711fd0c2a021e6b3ecaed4478ef7880ad
Haritha Yerukonda <107812251+HarithaYerukonda@users.noreply.github.com>
2025-08-11 12:46:20 +0100

    Create testData.js

---
+++ testData.js
@@ -0,0 +1,7 @@
+module.exports = {
+    validUser: { username: 'standard_user', password: 'secret_sauce' },
+    invalidUser: { username: 'fake_user', password: 'wrong_pass' },
+    lockedOutUser: { username: 'locked_out_user', password: 'secret_sauce' },
+    problemUser: { username: 'problem_user', password: 'secret_sauce' },
+    performanceUser: { username: 'performance_glitch_user', password: 'secret_sauce' }
+  };
